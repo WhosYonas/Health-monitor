bd35ebf54b149bf454978e6fe4d3c04de111a483
yonash <yonash@kth.se>
2026-04-20 09:54:17 +0000

    Started on the structure

---
+++ Backend/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config.settings import db as db_url
+from dotenv import load_dotenv
+import os
+
+# ---------------- Database Setup ----------------
+load_dotenv()
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
