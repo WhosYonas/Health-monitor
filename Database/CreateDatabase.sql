7a3f740e77235836e6dfbd6a8d07d21f88cd8b85
Matteus Isa Merhane <maim@kth.se>
2026-04-24 21:30:29 +0200

    added relatives and removed tables

--- Database/CreateDatabase.sql
+++ Database/CreateDatabase.sql
@@ -7,14 +7,11 @@ CREATE TABLE person (
     role VARCHAR(500) NOT NULL
 );
 
-ALTER TABLE person
-ADD CONSTRAINT PK_person PRIMARY KEY (person_id);
+ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (person_id);
 
-ALTER TABLE person
-ADD CONSTRAINT UQ_person_personummer UNIQUE (personnummer);
+ALTER TABLE person ADD CONSTRAINT UQ_person_personummer UNIQUE (personnummer);
 
-ALTER TABLE person
-ADD CONSTRAINT CHK_person_role CHECK(role IN('patient', 'caregiver'));
+ALTER TABLE person ADD CONSTRAINT CHK_person_role CHECK(role IN('patient', 'caregiver'));
 
 CREATE TABLE account (
     account_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
@@ -23,44 +20,36 @@ CREATE TABLE account (
     person_id INT NOT NULL
 );
 
-ALTER TABLE account
-ADD CONSTRAINT PK_account PRIMARY KEY (account_id);
+ALTER TABLE account ADD CONSTRAINT PK_account PRIMARY KEY (account_id);
 
-ALTER TABLE account
-ADD CONSTRAINT UQ_account_username UNIQUE (username_id);
+ALTER TABLE account ADD CONSTRAINT UQ_account_username UNIQUE (username_id);
 
-ALTER TABLE account
-ADD CONSTRAINT uq_account_person UNIQUE (person_id);
+ALTER TABLE account ADD CONSTRAINT uq_account_person UNIQUE (person_id);
 
 CREATE TABLE caregiver (
     caregiver_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     person_id INT
 );
 
-ALTER TABLE caregiver
-ADD CONSTRAINT PK_caregiver PRIMARY KEY (caregiver_id);
+ALTER TABLE caregiver ADD CONSTRAINT PK_caregiver PRIMARY KEY (caregiver_id);
 
-ALTER TABLE caregiver
-ADD CONSTRAINT UQ_caregiver_person UNIQUE (person_id);
+ALTER TABLE caregiver ADD CONSTRAINT UQ_caregiver_person UNIQUE (person_id);
 
 CREATE TABLE patient (
     patient_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     person_id INT NOT NULL
 );
 
-ALTER TABLE patient
-ADD CONSTRAINT PK_patient PRIMARY KEY (patient_id);
+ALTER TABLE patient ADD CONSTRAINT PK_patient PRIMARY KEY (patient_id);
 
-ALTER TABLE patient
-ADD CONSTRAINT UQ_patient_person_id UNIQUE (person_id);
+ALTER TABLE patient ADD CONSTRAINT UQ_patient_person_id UNIQUE (person_id);
 
 CREATE TABLE device (
     device_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     patient_id INT NOT NULL
 );
 
-ALTER TABLE device
-ADD CONSTRAINT PK_device PRIMARY KEY (device_id);
+ALTER TABLE device ADD CONSTRAINT PK_device PRIMARY KEY (device_id);
 
 CREATE TABLE measurement (
     measurement_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
@@ -89,17 +78,12 @@ ALTER TABLE relatives ADD CONSTRAINT PK_relatives PRIMARY KEY (relatives_id);
 ALTER TABLE relatives ADD CONSTRAINT FK_relatives_patient FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
 ON DELETE CASCADE;
 
-
 ALTER TABLE account ADD CONSTRAINT FK_account_0 FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE;
 
-ALTER TABLE caregiver
-ADD CONSTRAINT FK_caregiver_0 FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE;
+ALTER TABLE caregiver ADD CONSTRAINT FK_caregiver_0 FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE;
 
-ALTER TABLE patient
-ADD CONSTRAINT FK_patient_0 FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE;
+ALTER TABLE patient ADD CONSTRAINT FK_patient_0 FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE;
 
-ALTER TABLE device
-ADD CONSTRAINT FK_device_0 FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE;
+ALTER TABLE device ADD CONSTRAINT FK_device_0 FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE;
 
-ALTER TABLE measurement
-ADD CONSTRAINT FK_measurement_0 FOREIGN KEY (device_id) REFERENCES device (device_id) ON DELETE CASCADE;
+ALTER TABLE measurement ADD CONSTRAINT FK_measurement_0 FOREIGN KEY (device_id) REFERENCES device (device_id) ON DELETE CASCADE;
