c7e342eb32fe71b0a27115139e1edd84f6a35c13
Matteus Isa Merhane <maim@kth.se>
2026-04-24 15:10:58 +0200

    updated the astah diagram and the sql script

--- Database/CreateDatabase.sql
+++ Database/CreateDatabase.sql
@@ -1,68 +1,74 @@
+
 CREATE TABLE person (
- person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- first_name VARCHAR(500),
- last_name VARCHAR(500),
- phone_number VARCHAR(500)
+    person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    first_name VARCHAR(500),
+    last_name VARCHAR(500),
+    phone_number VARCHAR(500),
+    personnummer VARCHAR(500),
+    role VARCHAR(50)
 );
 
 ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (person_id);
 
 
 CREATE TABLE account (
- account_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- personummer VARCHAR(500),
- password_hashed VARCHAR(500),
- person_id INT NOT NULL
+    account_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    username_id VARCHAR(500),
+    password_hashed VARCHAR(500),
+    person_id INT
 );
 
 ALTER TABLE account ADD CONSTRAINT PK_account PRIMARY KEY (account_id);
 
 
-CREATE TABLE device (
- device_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- person_id INT NOT NULL
+CREATE TABLE caregiver (
+    caregiver_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    person_id INT
 );
 
-ALTER TABLE device ADD CONSTRAINT PK_Device PRIMARY KEY (device_id);
+ALTER TABLE caregiver ADD CONSTRAINT PK_caregiver PRIMARY KEY (caregiver_id);
+
+
+CREATE TABLE patient (
+    patient_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    person_id INT NOT NULL
+);
+
+ALTER TABLE patient ADD CONSTRAINT PK_patient PRIMARY KEY (patient_id);
+
+
+CREATE TABLE device (
+    device_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    patient_id INT
+);
+
+ALTER TABLE device ADD CONSTRAINT PK_device PRIMARY KEY (device_id);
 
 
 CREATE TABLE measurement (
- measurement_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- time TIMESTAMP WITH TIME ZONE,
- blood_oxygen INT,
- heart_rate INT,
- temperature INT,
- device_id INT NOT NULL
+    measurement_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    time TIMESTAMP WITH TIME ZONE,
+    blood_oxygen DECIMAL(10),
+    heart_rate INT,
+    temperature DECIMAL(10),
+    device_id INT
 );
 
 ALTER TABLE measurement ADD CONSTRAINT PK_measurement PRIMARY KEY (measurement_id);
 
 
-
 ALTER TABLE account ADD CONSTRAINT FK_account_0 FOREIGN KEY (person_id) REFERENCES person (person_id)
 ON DELETE CASCADE;
 
-ALTER TABLE Device ADD CONSTRAINT FK_Device_0 FOREIGN KEY (person_id) REFERENCES person (person_id)
+ALTER TABLE caregiver ADD CONSTRAINT FK_caregiver_0 FOREIGN KEY (person_id) REFERENCES person (person_id)
 ON DELETE CASCADE;
 
-ALTER TABLE measurement ADD CONSTRAINT FK_measurement_0 FOREIGN KEY (device_id) REFERENCES Device (device_id)
+
+ALTER TABLE patient ADD CONSTRAINT FK_patient_0 FOREIGN KEY (person_id) REFERENCES person (person_id)
 ON DELETE CASCADE;
 
-CREATE OR REPLACE FUNCTION delete_person_when_account_deleted()
-RETURNS TRIGGER AS $$
-BEGIN
-    DELETE FROM person
-    WHERE person_id = OLD.person_id;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
+ALTER TABLE device ADD CONSTRAINT FK_device_0 FOREIGN KEY (patient_id) REFERENCES patient (patient_id)
+ON DELETE CASCADE;
 
-DROP TRIGGER IF EXISTS trg_delete_person_after_account_delete ON account;
-
-CREATE TRIGGER trg_delete_person_after_account_delete
-AFTER DELETE ON Account
-FOR EACH ROW
-EXECUTE FUNCTION delete_person_when_account_deleted();
-
-
-
+ALTER TABLE measurement ADD CONSTRAINT FK_measurement_0 FOREIGN KEY (device_id) REFERENCES device (device_id)
+ON DELETE CASCADE;
