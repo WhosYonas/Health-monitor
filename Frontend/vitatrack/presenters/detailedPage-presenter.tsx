c6a4bda85d52f1aaef888df9185366c3ddd5c7a6
alimsmiley <138820344+alimsmiley@users.noreply.github.com>
2026-04-27 12:36:17 +0200

    feat: graph added with muck up data

--- Frontend/vitatrack/presenters/detailedPage-presenter.tsx
+++ Frontend/vitatrack/presenters/detailedPage-presenter.tsx
@@ -1,20 +1,29 @@
 "use client";
 
-import {DetailedPage} from '../views/detailedPage';
-import {useState} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {AppDispatch, RootState} from '@/lib/store';
+import { DetailedPage } from "../views/detailedPage";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "@/lib/store";
+import { useEffect } from "react";
 
+import { postGetPatientHealthDataThunk } from "@/communication/patientHealthDataCommunication";
 
-export function DetailedPagePresenter(){
-    const dispatch = useDispatch<AppDispatch>();
-    const healthData = useSelector((state: RootState) => state.user.health_data);
-    const { loading, error_message } = useSelector((state: RootState) => state.user,);
-    return (
-        <DetailedPage 
-            healthData={healthData}
-            loading={loading}
-            errorMessage={error_message}
-        />
-    )
+export function DetailedPagePresenter() {
+  const dispatch = useDispatch<AppDispatch>();
+  const patient = useSelector((state: RootState) => state.patient);
+  const personNumber = useSelector(
+    (state: RootState) => patient.patient_info?.person_number,
+  );
+
+  useEffect(() => {
+    if (personNumber) {
+      dispatch(postGetPatientHealthDataThunk({ person_number: personNumber }));
+    }
+  }, [dispatch, personNumber]);
+  return (
+    <DetailedPage
+      healthData={patient.health_data}
+      loading={patient.healthLoading}
+      errorMessage={patient.health_error_message}
+    />
+  );
 }
