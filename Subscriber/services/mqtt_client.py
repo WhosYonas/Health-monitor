8bfa36206920fb83d06cb27d4e848db551e339ce
yonash <yonash@kth.se>
2026-05-09 22:21:03 +0000

    Update mqtt client

--- Subscriber/services/mqtt_client.py
+++ Subscriber/services/mqtt_client.py
@@ -29,6 +29,11 @@ def on_disconnect(client, userdata, rc):
 
 def create_client() -> paho_client.Client:
     client = paho_client.Client()
+    client.tls_set(
+        ca_certs=str(mqtt_settings.ca_cert),
+        certfile=str(mqtt_settings.client_cert),
+        keyfile=str(mqtt_settings.client_key),
+    )
     client.on_connect = on_connect
     client.on_message = on_message
     client.on_disconnect = on_disconnect
