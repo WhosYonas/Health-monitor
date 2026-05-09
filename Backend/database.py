74017f58ec6e931b4e2dab3cfd649c54cc5f0984
yonash <yonash@kth.se>
2026-05-09 22:16:42 +0000

    Update db.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,26 +1,22 @@
-import sys
-from pathlib import Path
-
-from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-sys.path.append("../Subscriber")
+host = os.environ["DATABASE_HOST"]
+port = os.environ["DATABASE_PORT"]
+name = os.environ["DATABASE_NAME"]
+user = os.environ["DATABASE_USER"]
+password = os.environ["DATABASE_PASSWORD"]
 
-from config.settings import db as db_url
+dsn = f"postgresql://{user}:{password}@{host}:{port}/{name}"
 
-# ---------------- Database Setup ----------------
-load_dotenv(Path(__file__).resolve().parent.parent / "Subscriber" / ".env")
-
-engine = create_engine(db_url.dsn)
+engine = create_engine(dsn)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
-
 def init_db():
     Base.metadata.create_all(bind=engine)
-
 
 def get_db():
     db = SessionLocal()
